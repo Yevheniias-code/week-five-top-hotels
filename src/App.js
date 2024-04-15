0148ba084972aa020107389b8c7bd83687a6bff4
yevheniias-code <jewgenia.lazebnyk@icloud.com>
2024-04-15 15:04:28 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+  const [hotels, setHotels] = useState(data);
+
+  const remoweHotel = (id) => {
+    let newHotels = hotels.filter(hotel => hotel.id !== id);
+    setHotels(newHotels);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <h1>NYC top {hotels.length} hotels</h1>
+      </div>
+
+      {hotels.map(element => {
+        const{id, hotelName, description, image, source} = element;
+
+        return(
+          <div key={id}>
+            <div className='container'>
+              <h2>{id} - {hotelName}</h2>
+            </div>
+
+            <div className='container'>
+              <p>{description}</p>
+            </div>
+
+            <div className='container'>
+              <img src={image} width='400px' alt='Hotel' />
+            </div>
+
+            <div className='container'>
+              <p>Source: {source}</p>
+            </div>
+
+            <div className='container'>
+              <button onClick={()=> remoweHotel(id)}>Remove</button>
+            </div>
+          </div>
+        )
+      })}
+
+      <div className='container'>
+        <button onClick={() => setHotels([])}>Delete all</button>
+      </div>
     </div>
   );
 }
